dcc06071805b7f9a05c024bbb9a24dd1a8c42d05
Mohamed Safdar <moh.safdar@outlook.com>
2024-06-24 01:07:34 +0500

    authenticatin added using convex and clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://apt-cub-80.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+
+interface ConvexClientProviderProps {
+  children: React.ReactNode;
+}
+
+const convexURl = process.env.NEXT_PUBLIC_CONVEX_URL;
+
+const convex = new ConvexReactClient(convexURl!);
+
+export const ConvexClientProvider = ({
+  children,
+}: ConvexClientProviderProps) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
